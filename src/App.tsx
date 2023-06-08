04c41902ecf737193d4fd2883b437783388d4557
Atavaliev Timur <tatavaliev@gmail.com>
2023-06-08 15:55:51 +0600

    Create structure of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import UserForm from "./components/UserForm/UserForm";
+import Users from "./components/Users/Users";
 
 function App() {
-  return (
-    <div className="App">
-App
-    </div>
-  );
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col  border border-dark">
+                    <UserForm/>
+                </div>
+                <div className="col border border-dark">
+                    <Users/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
